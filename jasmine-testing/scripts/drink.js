aea8ec79bf3bf021d4b464317dad163202f439cf
Nicolette Shaw <nikkikobako@gmail.com>
2020-09-29 07:56:43 +0000

    Added first test

---
+++ jasmine-testing/scripts/drink.js
@@ -0,0 +1,4 @@
+let age = 0;
+function whatCanIDrink(age){
+    return age;
+}
---
+++ jasmine-testing/spec/drinkSpec.js
@@ -0,0 +1,9 @@
+
+describe ("whatCanIDrink", function() {
+    describe ("age tests", function() {
+        it ("should create a new string with either single or double quotes", 
+            function() {
+                  expect("Sorry. I can’t tell what drink because that age is incorrect!").toEqual(null);
+               });
+});
+});
