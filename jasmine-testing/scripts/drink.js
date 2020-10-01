6421299800df2db8c791ff7c6391c89eeca2abb4
Nicolette Shaw <nikkikobako@gmail.com>
2020-10-01 07:50:53 +0000

    The actual solution from code institute

--- jasmine-testing/scripts/drink.js
+++ jasmine-testing/scripts/drink.js
@@ -1,11 +1,16 @@
-let age;
-function whatCanIDrink(age){
+whatCanIDrink = function(age) {
     if (age < 0) {
         return "Sorry. I can’t tell what drink because that age is incorrect";
     } else if  ( age < 14){
          return "Drink Toddy";
     } else if (age < 18) {
         return "Drink Coke";
+    } else if (age < 21) {
+        return "Drink Beer";
+    }
+     else if (age < 130) {
+        return "Drink Whisky";
+    } else {
+        return "Sorry. I can’t tell what drink because that age is incorrect!"
     }
 }
-whatCanIDrink(1);
